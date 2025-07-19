ef0f153b0dc5853f3f6b87fb13a88b4b788a7cd9
Beejae <beebanq@gmail.com>
2025-07-19 15:18:56 +0100

    Remove abis folder and its contents from the repository

--- abis/PrizeTypes.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-library PrizeTypes {
-    enum Standard { ERC721, ERC1155, ERC20, ETH }
-    enum RevealType { Instant, Manual, Scheduled }
-}
